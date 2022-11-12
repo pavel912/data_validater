12bc326b39d899e429cb2c9d9bdfaf63daf7396e
pavel912 <gta4thebest@mail.ru>
2022-11-12 21:34:16 +0300

    add nested checks in maps

--- app/src/main/java/hexlet/code/Validator.java
+++ app/src/main/java/hexlet/code/Validator.java
@@ -5,9 +5,6 @@ import hexlet.code.schemas.NumberSchema;
 import hexlet.code.schemas.StringSchema;
 
 public class Validator {
-    public Validator() {
-
-    }
 
     public StringSchema string() {
         return new StringSchema();
--- app/src/main/java/hexlet/code/schemas/MapSchema.java
+++ app/src/main/java/hexlet/code/schemas/MapSchema.java
@@ -1,6 +1,7 @@
 package hexlet.code.schemas;
 
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 public class MapSchema extends BaseSchema {
@@ -10,7 +11,19 @@ public class MapSchema extends BaseSchema {
     }
 
     public MapSchema sizeof(int size) {
-        operator = operator.and(value -> ((HashMap) value).size() >= size);
+        operator = operator.and(value -> ((HashMap<?, ?>) value).size() >= size);
+        return this;
+    }
+
+    public MapSchema shape(Map<String, BaseSchema> schemas) {
+        operator = operator
+                .and(checkedMap -> ((HashMap<?, ?>) checkedMap)
+                        .keySet() //get keySet of checked Map
+                        .stream()
+                        .allMatch(checkedMapKey -> schemas // check every key
+                                .get(checkedMapKey) // get Schema of a key
+                                .isValid(((HashMap) checkedMap)
+                                        .get(checkedMapKey)))); // check if value of a key is valid
         return this;
     }
 }
