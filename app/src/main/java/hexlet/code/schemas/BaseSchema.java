2b18dcbb9ad28390b8204c8fd14f59d211bf2b90
pavel912 <gta4thebest@mail.ru>
2022-11-12 22:30:07 +0300

    fix final v2

--- app/src/main/java/hexlet/code/schemas/BaseSchema.java
+++ app/src/main/java/hexlet/code/schemas/BaseSchema.java
@@ -5,12 +5,12 @@ import java.util.function.Predicate;
 public abstract class BaseSchema {
     private Predicate<Object> operator = value -> true;
 
-    public Predicate<Object> getOperator() {
+    public final Predicate<Object> getOperator() {
         return operator;
     }
 
-    public void setOperator(Predicate<Object> operator) {
-        this.operator = operator;
+    public final void setOperator(Predicate<Object> value) {
+        this.operator = value;
     }
 
     public final boolean isValid(Object value) {
