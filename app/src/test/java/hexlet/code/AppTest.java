9d2572939efcff93d66e130978a54fd88de5237c
pavel912 <gta4thebest@mail.ru>
2022-11-12 21:33:00 +0300

    add tests for nested checks

--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -1,5 +1,6 @@
 package hexlet.code;
 
+import hexlet.code.schemas.BaseSchema;
 import hexlet.code.schemas.MapSchema;
 import hexlet.code.schemas.NumberSchema;
 import hexlet.code.schemas.StringSchema;
@@ -28,6 +29,11 @@ public class AppTest {
         assertTrue(schema.isValid("hexlet"));
         assertFalse(schema.isValid(null));
         assertFalse(schema.isValid(""));
+
+        schema.minLength(5);
+
+        assertTrue(schema.isValid("what does the fox say"));
+        assertFalse(schema.isValid("what"));
 
         assertTrue(schema.contains("wh").isValid("what does the fox say"));
         assertTrue(schema.contains("what").isValid("what does the fox say"));
@@ -72,7 +78,7 @@ public class AppTest {
         schema.required();
 
         assertFalse(schema.isValid(null));
-        assertTrue(schema.isValid(new HashMap()));
+        assertTrue(schema.isValid(new HashMap<>()));
 
         Map<String, String> data = new HashMap<>();
         data.put("key1", "value1");
@@ -84,5 +90,37 @@ public class AppTest {
         data.put("key2", "value2");
 
         assertTrue(schema.isValid(data));
+    }
+
+    @Test
+    public void testNestedSchemas() {
+        Validator v = new Validator();
+
+        MapSchema schema = v.map();
+
+        Map<String, BaseSchema> schemas = new HashMap<>();
+        schemas.put("name", v.string().required());
+        schemas.put("age", v.number().positive());
+        schema.shape(schemas);
+
+        Map<String, Object> human1 = new HashMap<>();
+        human1.put("name", "Kolya");
+        human1.put("age", 100);
+        assertTrue(schema.isValid(human1));
+
+        Map<String, Object> human2 = new HashMap<>();
+        human2.put("name", "Maya");
+        human2.put("age", null);
+        assertTrue(schema.isValid(human2));
+
+        Map<String, Object> human3 = new HashMap<>();
+        human3.put("name", "");
+        human3.put("age", null);
+        assertFalse(schema.isValid(human3));
+
+        Map<String, Object> human4 = new HashMap<>();
+        human4.put("name", "Valya");
+        human4.put("age", -5);
+        assertFalse(schema.isValid(human4));
     }
 }
