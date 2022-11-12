bc735d05fc06ec5e10a7067fd928bc665abd3e75
pavel912 <gta4thebest@mail.ru>
2022-11-12 22:06:42 +0300

    fix numeric value check

--- app/src/main/java/hexlet/code/schemas/NumberSchema.java
+++ app/src/main/java/hexlet/code/schemas/NumberSchema.java
@@ -12,14 +12,14 @@ public class NumberSchema extends BaseSchema {
 
     public NumberSchema positive() {
         operator = operator
-                .and(value -> value == null || (int) value > 0);
+                .and(value -> value == null || !(value instanceof Integer) || (int) value > 0);
         return this;
     }
 
     public NumberSchema range(int start, int end) {
         operator = operator
-                .and(value -> value == null || (int) value >= start)
-                .and(value -> value == null || (int) value <= end);
+                .and(value -> value == null || !(value instanceof Integer) || (int) value >= start)
+                .and(value -> value == null || !(value instanceof Integer) || (int) value <= end);
         return this;
     }
 }
