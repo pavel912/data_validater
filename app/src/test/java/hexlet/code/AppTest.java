22ab0926ab3dc906382d1e1d495d4f556567ab2f
pavel912 <gta4thebest@mail.ru>
2022-11-12 16:04:10 +0300

    initialize a repo

---
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -0,0 +1,13 @@
+package hexlet.code;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AppTest {
+
+    @Test
+    public void test() {
+        assertEquals(0, 0);
+    }
+}
