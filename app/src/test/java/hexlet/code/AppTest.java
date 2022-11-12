b66ad6028e02a86231697d2e3c1a2679d2c135d7
pavel912 <gta4thebest@mail.ru>
2022-11-12 18:42:44 +0300

    add tests for string schema

--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -2,12 +2,31 @@ package hexlet.code;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class AppTest {
 
     @Test
-    public void test() {
-        assertEquals(0, 0);
+    public void testStringSchema() {
+        Validator v = new Validator();
+
+        StringSchema schema = v.string();
+
+        assertTrue(schema.isValid(""));
+        assertTrue(schema.isValid(null));
+
+        schema.required();
+
+        assertTrue(schema.isValid("what does the fox say"));
+        assertTrue(schema.isValid("hexlet"));
+        assertFalse(schema.isValid(null));
+        assertFalse(schema.isValid(""));
+
+        assertTrue(schema.contains("wh").isValid("what does the fox say"));
+        assertTrue(schema.contains("what").isValid("what does the fox say"));
+        assertFalse(schema.contains("whatthe").isValid("what does the fox say"));
+
+        assertFalse(schema.isValid("what does the fox say"));
     }
 }
